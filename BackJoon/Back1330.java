0136dfda2cfb2782a31ff24edbee7195e1d8d800
ktk8916 <ktk8916@naver.com>
2022-11-20 19:38:19 +0900

    백준 1330번

---
+++ BackJoon/Back1330.java
@@ -0,0 +1,15 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class Back1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if(num1>num2) System.out.print('>');
+        if(num1<num2) System.out.print('<');
+        if(num1==num2) System.out.print("==");
+    }
+}
