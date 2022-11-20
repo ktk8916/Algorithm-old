202ecf8ea0464b363bed2577642e6cae7ab8d99e
ktk8916 <ktk8916@naver.com>
2022-11-20 22:02:39 +0900

    백준 5597번

---
+++ BackJoon/P5597.java
@@ -0,0 +1,21 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class P5597 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean[] submit = new boolean[30];
+
+        for(int i=0;i<28;i++){
+            submit[sc.nextInt()-1] = true;
+        }
+
+        for(int i=0;i<30;i++){
+            if(submit[i]==false){
+                System.out.println(i+1);
+            }
+        }
+    }
+}
